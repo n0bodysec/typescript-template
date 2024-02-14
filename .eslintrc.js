85898796dd083495ba740b7dd2e22b52bd2a521b
Nobody <17956512+n0bodysec@users.noreply.github.com>
2024-02-14 02:06:32 -0300

    chore: update tsconfig.json and .eslintrc.js
    deps: update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,18 @@
 module.exports = {
-	plugins: ['@typescript-eslint'],
+	root: true,
+	env: { node: true },
 	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint/eslint-plugin'],
+	parserOptions: {
+		project: 'tsconfig.json',
+		tsconfigRootDir: __dirname,
+		sourceType: 'module',
+	},
+	ignorePatterns: ['node_modules', 'dist'],
 	extends: [
 		'@n0bodysec',
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended-type-checked',
 		'plugin:import/typescript',
 	],
-	parserOptions: {
-		project: true,
-		tsconfigRootDir: __dirname,
-	},
-	root: true,
-	ignorePatterns: ['node_modules', 'dist'],
 };
