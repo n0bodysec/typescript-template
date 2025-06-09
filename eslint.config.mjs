01d0ab17a1656ef9461f96bec71bbf0f9ce9aa66
Agustín <17956512+snowyyd@users.noreply.github.com>
2025-06-08 23:42:07 -0300

    refactor: update tsconfig
    deps: update dependencies
    fix: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,11 @@
 import configX from '@snowyyd/eslint-config/base';
 import tseslint from 'typescript-eslint';
-import tsconfig from './tsconfig.json';
 
 export default tseslint.config(
 	...configX,
 	...tseslint.configs.recommendedTypeChecked,
 	...tseslint.configs.stylisticTypeChecked,
 	{
-		ignores: tsconfig.exclude,
 		languageOptions: {
 			parserOptions: {
 				projectService: {
